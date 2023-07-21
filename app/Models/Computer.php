31b55fb878bee28719b5a171a342cd6742b50f9d
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-07-21 16:11:51 +0200

    Model e $fillable

--- app/Models/Computer.php
+++ app/Models/Computer.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Computer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'brand',
+        'type',
+        'cooling',
+        'price',
+        'release_date',
+        'description'
+    ];
 }
